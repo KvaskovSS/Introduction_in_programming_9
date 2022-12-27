40c67e082fc5cf934472a93ead82c75b549b55a6
KvaskovSS <sava0980@yandex.ru>
2022-12-27 16:18:33 +0300

    Task 2

--- Program.cs
+++ Program.cs
@@ -1,23 +1,26 @@
-﻿int decimalToBinary(int dec){
-    if(dec == 0){
-        return 0;
-    }else{
-        return dec % 2 + 10 * (decimalToBinary(dec / 2));
-    }
-}
+﻿// Task 1
+// int decimalToBinary(int dec){
+//     if(dec == 0){
+//         return 0;
+//     }else{
+//         return dec % 2 + 10 * (decimalToBinary(dec / 2));
+//     }
+// }
 
+// Task 2
 void findNatureSum(int M, int N, int sum){
     if(M > N){
         Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
         return;
     }
-    
+    sum += M++;
+    findNatureSum(M, N, sum);
 }
 
 // Task 1
-Console.WriteLine("Введите десятичное число : ");
-int dec = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(decimalToBinary(dec));
+// Console.WriteLine("Введите десятичное число : ");
+// int dec = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(decimalToBinary(dec));
 
 // Task 2
 Console.WriteLine("Введите начальное число M:");
